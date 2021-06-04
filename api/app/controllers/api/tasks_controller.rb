59a276f996525785e43bb3bde51ecbab77860c67
KentaroYamada91011 <kentaro.10.11@icloud.com>
2021-06-04 15:15:44 +0900

    add: rubo cop 追加 #step7

--- api/app/controllers/api/tasks_controller.rb
+++ api/app/controllers/api/tasks_controller.rb
@@ -1,41 +1,43 @@
-class Api::TasksController < ApplicationController
-  before_action :set_task, only: [:destroy, :update]
+class Api
+  class TasksController < ApplicationController
+    before_action :set_task, only: %i[destroy update]
 
-  def index
-    @tasks = Task.all
-    render json: @tasks
-  end
+    def index
+      @tasks = Task.all
+      render json: @tasks
+    end
 
-  def create
-    task = Task.create!(task_param)
-    render json: {  status: 'SUCCESS', data: task }
-    rescue => e
+    def create
+      task = Task.create!(task_param)
+      render json: { status: 'SUCCESS', data: task }
+    rescue ActiveRecord::RecordInvalid => e
       render json: { status: 'ERROR', message: e }
-  end
+    end
 
-  def destroy
-    @task.destroy
-    render json: { status: 'SUCCESS', message: 'Delete the task', data: @task}
-    rescue => e
+    def destroy
+      @task.destroy
+      render json: { status: 'SUCCESS', message: 'Delete the task', data: @task }
+    rescue ActiveRecord::RecordInvalid => e
       render json: { status: 'ERROR', message: e }
-  end
+    end
 
-  def update
-    @task.update!(task_param)
-    render json: {  status: 'SUCCESS', data: @task }
-    rescue => e
+    def update
+      @task.update!(task_param)
+      render json: { status: 'SUCCESS', data: @task }
+    rescue ActiveRecord::RecordInvalid => e
       render json: { status: 'ERROR', message: e }
-  end
+    end
 
-  private
-  def set_task
-    @task = Task.find(params[:id])
-  end
+    private
 
-  def task_param
-    data = params.fetch(:task, {}).permit(
-      :user_id, :title, :description, :deadline, :status, :parent_id
-    )
-    data
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    def task_param
+      params.fetch(:task, {}).permit(
+        :user_id, :title, :description, :deadline, :status, :parent_id
+      )
+    end
   end
 end
