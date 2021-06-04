a8929154099e4c6952a5a16ef30a5f418941f6d4
KentaroYamada91011 <kentaro.10.11@icloud.com>
2021-06-04 09:37:13 +0900

    add: seed data #step7

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(user_id: 1, title: "サンプルプロジェクト", description: "サンプル", deadline: "2021-05-31 10:21:32", status: 10, parent_id: 0)
