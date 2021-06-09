264d6a9dd02cb19132c2dc4ca1953ed2fad226bd
KentaroYamada91011 <kentaro.10.11@icloud.com>
2021-06-09 14:39:07 +0900

    add: model validation #step11

--- api/app/models/task.rb
+++ api/app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   enum status: { "not_started": 10, "in_progress": 20, "done": 30 }, _prefix: true
+  validates :title, presence: true, length: { maximum: 30 }, allow_blank: false
+  validates :description, length: { maximum: 255 }
 end
