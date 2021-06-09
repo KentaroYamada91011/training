62303190357dac604981983194d4fb0aa22471f9
KentaroYamada91011 <kentaro.10.11@icloud.com>
2021-06-09 14:52:17 +0900

    model 内のメッセージ修正

--- api/app/models/task.rb
+++ api/app/models/task.rb
@@ -1,3 +1,3 @@
 class Task < ApplicationRecord
-  enum status: { "not_started": 10, "in_progress": 20, "done": 30 }, _prefix: true
+  enum status: { I18n.t("status.not_started") => 10, I18n.t("status.in_progress") => 20, I18n.t("status.done") => 30 }, _prefix: true
 end
